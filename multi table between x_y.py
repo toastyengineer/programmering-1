db396578453508e8b4a3ee19150b8907ac155926
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2018-10-10 16:01:07 +0200

    multiplication table m-n finish
    -multi table between x_y finally working

--- multi table between x_y.py
+++ multi table between x_y.py
@@ -4,6 +4,8 @@ n = int(input("enter nm 2: "))
 
 for inputs in range(m, n+1):
     for factor in range(1, 11):
+        if inputs*factor < 10:
+            print(end=" ")
         if factor < 10:
             print(inputs*factor, end=", ")
         else:
