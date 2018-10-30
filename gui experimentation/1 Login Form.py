e370b9903bc5f1bd73b9a70323638cf11fd959ff
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2018-10-30 02:12:41 +0100

    added functionality
    -log in function
    -register function placeholder
    -checkbox is checkable
    -password entry now hidden with *

--- gui experimentation/1 Login Form.py
+++ gui experimentation/1 Login Form.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import tkinter.messagebox
 
-#Confirm on exit
+user1 = "root"
+pass1 = "password"
 
 
 def confirm_exit():
@@ -10,28 +11,49 @@ def confirm_exit():
         exit()
 
 
+def log_in():
+    if entry_user.get() == user1 and entry_pass.get() == pass1:
+        tkinter.messagebox.showinfo("Info", "Successfully logged in!")
+    elif entry_user.get() == "" or entry_pass.get() == "":
+        tkinter.messagebox.showinfo("Info", "You did not enter a valid password and/or username.")
+    else:
+        tkinter.messagebox.showwarning("Warning", "Entered username or password was incorrect.\nIs caps-lock disabled?")
+
+
+def register():
+    print("Unavailable")
+
+
+def checkbox():
+    checkbox_state = checky.get()
+
+
 root = Tk()
-root.title("PK")
-#Username and Password label
+root.title("PQ")
+root.resizable(False, False)
+# Username and Password label
 user = Label(root, text="Username: ")
 user.grid(row=0, sticky=E)
 password = Label(root, text="Password: ")
 password.grid(row=1, sticky=E)
 
-#Username and Password entries
+# Username and Password entries
 entry_user = Entry(root)
 entry_user.grid(row=0, column=1)
-entry_pass = Entry(root)
+entry_pass = Entry(root, show='*')
 entry_pass.grid(row=1, column=1)
 
-#Checkbox
-c = Checkbutton(root, text="Keep me logged in")
+# Checkbox
+checky = IntVar()
+c = Checkbutton(root, text="Keep me logged in", variable=checky)
 c.grid(row=2, columnspan=2)
 
-#Buttons
-login = Button(root, text="Login")
-login.grid(row=3, column=1, pady=5, padx=10, sticky=E)
+# Buttons
 cancel = Button(root, text="Cancel", command=confirm_exit)
-cancel.grid(row=3, column=0)
+cancel.grid(row=3, column=0, pady=7, padx=10)
+register = Button(root, text="Register", command=register)
+register.grid(row=3, column=1, pady=7, padx=10)
+login = Button(root, text="Login", command=log_in)
+login.grid(row=3, column=2, pady=7, padx=10)
 
 root.mainloop()
