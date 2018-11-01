943842f37405152490f33dd378a26b3867fa9484
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2018-11-01 23:10:58 +0100

    added threading.py file
    -

--- experimentus maximus.py
+++ experimentus maximus.py
@@ -1,25 +1,6 @@
+def month_name(num):
+    num = num - 1
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return months[num]
 
-def bill(people, meals, drinks):
-    meals = people * meals * 70
-    drinks = people * drinks * 30
-    total = meals + drinks
-    print(total)
-
-
-def split_bill(total, people):
-    split = total / people
-    print(round(split*1.1, 2))
-
-
-def concatenate_strings(string1, string2):
-    print(string1 + string2)
-
-
-
-
-
-
-
-#bill(5, 2, 3)
-#split_bill(777, 5)
-#concatenate_strings("jätte","kul")
+print(month_name(12))
