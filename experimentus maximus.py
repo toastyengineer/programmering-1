00c954aad4e41a88185e068e17df47b262ae80db
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2018-11-02 15:34:20 +0100

    gitignore

--- experimentus maximus.py
+++ experimentus maximus.py
@@ -1,6 +1,9 @@
-def month_name(num):
-    num = num - 1
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    return months[num]
+def repeat(item, times):
+    output = []
+    while times > 0:
+        output.append(item)
+        times -= 1
+    return output
 
-print(month_name(12))
+
+print(repeat("5", 3))
