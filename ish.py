52ccb7667fd320f5832992e45054f009986b41e9
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2019-01-12 17:58:59 +0100

    Update ish.py

--- ish.py
+++ ish.py
@@ -1,11 +1,15 @@
 
-user_input = input("Enterish yourish wordish hereish: ")
+user_input = input("Universal Ish Translator\nEnterish yourish wordish hereish: ")
 word_list = user_input.split()
 
 for word in word_list:
 
     if word[-1] == "I" or word[-1] == "i":
         print(word, end=" ")
+
+    elif word[-3:] == "igt":
+        new = word.replace("igt", "ish")
+        print(new, end=" ")
 
     elif word[-1] == "?":
         word = word.replace("?", "")
