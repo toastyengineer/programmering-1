96cdd68473dfc441f0eee8afa3715aa4f82b12da
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2019-01-29 18:43:57 +0100

    test_rectangle added test

--- inlämningar/Moduler och funktioner/test_rectangle.py
+++ inlämningar/Moduler och funktioner/test_rectangle.py
@@ -13,6 +13,7 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(area(-3, 4), 12)
         self.assertEqual(area(1.5, 3.5), 5.25)
         self.assertEqual(area("hello", 3), "Error: TypeError")
+        self.assertEqual(area(4, "hello"), "Error: TypeError")
 
     def test_circ(self):
         """testing parameters for circ function"""
@@ -20,6 +21,7 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(circ(-5, -10), 30)
         self.assertEqual(circ(5.5, -2.5), 16)
         self.assertEqual(circ(6, "hello"), "Error: TypeError")
+        self.assertEqual(circ("hello", 3), "Error: TypeError")
 
 
 if __name__ == "__main__":
